be71af026cbdf29dab5e800b18d98d956879f9de
Peter Bright <drpizza@quiscalusmexicanus.org>
2013-05-02 17:58:55 +0100

    Win32 compatibility.

--- test-generic/test-generic.cpp
+++ test-generic/test-generic.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 
+#ifdef WIN32
+#include <Windows.h>
+#else
 #include <unistd.h>
+#endif
 
 #include "timer.hpp"
 
 int main() {
 	high_resolution_timer timer;
+#ifdef WIN32
+	Sleep(1000);
+#else
 	usleep(1000 * 1000);
+#endif
 	high_resolution_timer::duration dur = timer.pulse();
 	std::cerr << std::chrono::duration_cast<std::chrono::microseconds>(dur).count() << std::endl;
 	return 0;
